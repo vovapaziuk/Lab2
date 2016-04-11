595464d6da9b8e7f2df169f6a39bd2788b817c1d
vovapaziuk <htc60133@gmail.com>
2016-04-11 23:34:38 +0300

    hello world

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    Button buttonOne;
+
+    public void pressButton(ActionEvent actionEvent) {
+        buttonOne.setLayoutX(10);
+        System.out.println("1");
+    }
+
+
+}
