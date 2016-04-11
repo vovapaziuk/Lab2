68f20232314e420c46a6e76b14b292178a20b034
vovapaziuk <htc60133@gmail.com>
2016-04-11 23:55:22 +0300

    edit second

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,8 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
 
 public class Controller {
     @FXML
